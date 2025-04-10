844e9fa4b7aa0d79fed0636db74e3f8124b9d12c
Manuel Madrid <163127790+Shakeezy@users.noreply.github.com>
2025-04-10 18:50:04 +0200

    Delete pokerspot directory

--- pokerspot/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    receive_newsletter = models.BooleanField(default=False)
-    terms = models.BooleanField(default=False)
-    email = models.EmailField(unique=True)
-    def __str__(self):
-        return self.username
